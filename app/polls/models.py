5465db50a56fb8630dc1d8e18086c3c8ae68852a
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-10-14 12:34:48 +0900

    c

---
+++ app/polls/models.py
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+=======
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+>>>>>>> d8b7259034cf1638c1024c844ae353152f8a8d4d
+    votes = models.IntegerField(default=0)
