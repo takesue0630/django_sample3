1c4a61803eb502e766a0b1b5af4eb8d5d3b8a61f
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-10-14 04:57:06 +0000

    migrationのチュートリアル実施

--- app/polls/models.py
+++ app/polls/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.db import models
 
 
@@ -10,17 +9,4 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-=======
-from django.db import models
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
->>>>>>> d8b7259034cf1638c1024c844ae353152f8a8d4d
     votes = models.IntegerField(default=0)
